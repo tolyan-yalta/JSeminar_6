d6e57f0e99c5a1123c838f7df3c2bde319a8690e
aksdar31415@gmail.com <Zx38745!!!!!>
2022-11-17 00:42:13 +0300

    fix

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -8,18 +8,13 @@ import java.util.Map;
 public class Homework {
 
     // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-    // 2) Создать множество ноутбуков.
+    // 2) Создать коллекцию ноутбуков.
     // 3) Написать мапу, которая будет содержать критерий (или критерии) фильтрации и выведет
     //      ноутбуки, отвечающие фильтру.
-    //      Пример: 1 - ОЗУ, 2 - Объем ЖД, 3 - Операционная система, 4 - Цвет
+    //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
 
-    }
-
-    public Notebook createNotebook(){
-
-        return null;
     }
 
     public List<Notebook> initListNotebooks(){
@@ -27,7 +22,7 @@ public class Homework {
         return null;
     }
 
-    public List<Notebook> filter(Map<String, String> params){
+    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
 
         return null;
     }
