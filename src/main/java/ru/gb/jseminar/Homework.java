3c682ea0fc62d6b709eff8d6706811020a09d06c
anatoly <tolyan.yalta@yandex.ru>
2023-01-04 17:00:59 +0300

    Вариант с глюками для разбора проблемы

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -22,19 +22,19 @@ public class Homework {
 
     public List<Notebook> initListNotebooks(){
         List<Notebook> list = new ArrayList();
-        list.add(new Notebook(1, "Lenovo ThinkPad P73", "Intel Core i7", "16", "1000", "17.3", "Windows 10"));
-        list.add(new Notebook(2, "Dell Alienware M17-8123", "Intel Core i7", "16", "1000", "17.3", "Windows 10"));
-        list.add(new Notebook(3, "ASUS GL731GV", "Intel Core i7", "16", "1000", "17.3", "Windows 10"));
-        list.add(new Notebook(4, "ASUS VivoBook X513EA-BQ1608T", "Intel Core i5", "16", "1000", "15.6", "noOS"));
-        list.add(new Notebook(5, "Acer TravelMate P2510-G2-MG-31LF", "Intel Core i3", "4", "500", "15.6", "Windows 10"));
-        list.add(new Notebook(6, "Lenovo V130-15IKB", "Intel Core i3", "8", "1000", "15.6", "Windows 10"));
-        list.add(new Notebook(7, "Lenovo Legion Y530-15ICH", "Intel Core i5", "8", "1000", "15.6", "Windows 10"));
-        list.add(new Notebook(8, "Dell Inspiron 3511", "Intel Core i5", "8", "1000", "15.6", "Windows 11"));
+        list.add(new Notebook(1, "Lenovo ThinkPad P73", "Intel Core i7", 16, 1000, 17.3, "Windows 10"));
+        list.add(new Notebook(2, "Dell Alienware M17-8123", "Intel Core i7", 16, 1000, 17.3, "Windows 10"));
+        list.add(new Notebook(3, "ASUS GL731GV", "Intel Core i7", 16, 1000, 17.3, "Windows 10"));
+        list.add(new Notebook(4, "ASUS VivoBook X513EA-BQ1608T", "Intel Core i5", 16, 1000, 15.6, "noOS"));
+        list.add(new Notebook(5, "Acer TravelMate P2510-G2-MG-31LF", "Intel Core i3", 4, 500, 15.6, "Windows 10"));
+        list.add(new Notebook(6, "Lenovo V130-15IKB", "Intel Core i3", 8, 1000, 15.6, "Windows 10"));
+        list.add(new Notebook(7, "Lenovo Legion Y530-15ICH", "Intel Core i5", 8, 1000, 15.6, "Windows 10"));
+        list.add(new Notebook(8, "Dell Inspiron 3511", "Intel Core i5", 8, 1000, 15.6, "Windows 11"));
         return list;
     }
 
     public void filter(List<Notebook> notebooks){
-        Map<String, String> params = new HashMap<>();
+        Map<String, Object> params = new HashMap<>();
         System.out.println("Введите цифру, соответствующую необходимому критерию:");
         System.out.println("1 - Processor; 2 - RAM; 3 - HDD; 4 - Screen diagonal; 5 - Operating system");
         System.out.print("Введите цифру --> ");
@@ -48,21 +48,21 @@ public class Homework {
                 params.put("processor", processorList.get(proc));
                 break;
             case "2":
-                List<String> ramList = List.of("4", "8", "16");
+                List<Integer> ramList = List.of(4, 8, 16);
                 System.out.println("Выберите размер ОЗУ: 0 - 4 Gb; 1 - 8 Gb; 2 - 16 Gb");
                 System.out.print("Введите цифру --> ");
                 int ram = new Scanner(System.in).nextInt();
                 params.put("ram", ramList.get(ram));
                 break;
             case "3":
-                List<String> hddList = List.of("500", "1000");
+                List<Integer> hddList = List.of(500, 1000);
                 System.out.println("Выберите объем ЖД: 0 - 500 Gb; 1 - 1000 Gb");
                 System.out.print("Введите цифру --> ");
                 int hdd = new Scanner(System.in).nextInt();
                 params.put("hdd", hddList.get(hdd));
                 break;
             case "4":
-                List<String> screenDiagonalList = List.of("15.6", "17.3");
+                List<Double> screenDiagonalList = List.of(15.6, 17.3);
                 System.out.println("Выберите размер экрана: 0 - 15,6"+'"'+"; 1 - 17,3"+'"');
                 System.out.print("Введите цифру --> ");
                 int scr = new Scanner(System.in).nextInt();
