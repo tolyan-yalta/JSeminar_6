5f663a53032fc628dd7b63eeab4802810e02eb05
anatoly <tolyan.yalta@yandex.ru>
2023-01-04 13:43:57 +0300

    Add file Notebook.java

--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,45 @@
 package ru.gb.jseminar.data;
 
+import java.util.Map;
+
 public class Notebook {
+    Integer id;
+    String model;
+    String processor;
+    String ram;
+    String hdd;
+    String screenDiagonal;
+    String operatingSystem;
+
+    public Notebook(Integer i, String m, String p, String r, String h, String s, String o){
+        id = i;
+        model = m;
+        processor = p;
+        ram = r;
+        hdd = h;
+        screenDiagonal = s;
+        operatingSystem = o;
+    }
+
+    public  boolean comparison(Notebook item, Map params){
+        if (item.processor == params.get("processor")){
+            return true;
+        } else if (item.ram == params.get("ram")) {
+            return true;
+        } else if (item.hdd == params.get("hdd")) {
+            return true;
+        } else if (item.screenDiagonal == params.get("screenDiagonal")) {
+            return true;
+        } else if (item.operatingSystem == params.get("operatingSystem")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        String str;
+        str = "Model: " + model + "\tProcessor: " + processor + "\tRAM: " + ram + "Gb" + "\tHDD: " + hdd + "Gb" + "\tScreen diagonal: " + screenDiagonal + '"' + "\tOperating system: " + operatingSystem;
+        return str;
+    }
 }
